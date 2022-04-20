92a8aa7eb79ba54999ade80b2bc5a7da58b41dca
matt-cassinelli <67283034+matt-cassinelli@users.noreply.github.com>
2022-04-20 19:49:17 +0100

    Add latest challenges

---
+++ C# - CodeWars/6 kyu/RomanNumeralDecoder.cs
@@ -0,0 +1,76 @@
+/* -Roman Numerals Decoder-
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
+You don't need to validate the form of the Roman numeral.
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s.
+So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII).
+The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+Example:
+RomanDecode.Solution("XXI") -- should return 21
+Help:
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+*/
+
+using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic; // Needed for dictionary
+
+namespace CodeChallenges;
+
+public class RomanNumeralDecoder
+{
+    static int Solve(string input)
+    {
+        int output = 0;
+
+        var numerals = new Dictionary<char, int>
+        {
+            { 'I', 1    },
+            { 'V', 5    },
+            { 'X', 10   },
+            { 'L', 50   },
+            { 'C', 100  },
+            { 'D', 500  },
+            { 'M', 1000 }
+        };
+
+        foreach (var letter in input)
+        {
+            output += numerals[letter];
+        }
+
+        if (input.Contains("IV")|| input.Contains("IX"))
+            output -= 2;
+
+        if (input.Contains("XL")|| input.Contains("XC"))
+            output -= 20;
+
+        if (input.Contains("CD")|| input.Contains("CM"))
+            output -= 200;
+
+        return output;
+    }
+
+    [TestCase(21  , "XXI")]
+    [TestCase(1   , "I")]
+    [TestCase(2   ,"II")]
+    [TestCase(4   ,"IV")]
+    [TestCase(21  ,"XXI")]
+    [TestCase(500 ,"D")]
+    [TestCase(1000,"M")]
+    [TestCase(1954,"MCMLIV")]
+    [TestCase(1990,"MCMXC")]
+    [TestCase(2008,"MMVIII")]
+    [TestCase(2014,"MMXIV")]
+    public static void Test(int expectedOutput, string input)
+    {
+        Assert.AreEqual(expectedOutput, Solve(input));
+    }
+}
---
+++ C# - CodeWars/7 kyu/NumberSorter.cs
@@ -0,0 +1,115 @@
+/* -Sort Numbers-
+Finish the solution so that it sorts the passed in array of numbers.
+If the function passes in an empty array or null then it should return an empty array.
+*/
+
+using System;
+using NUnit.Framework;
+using System.Collections;
+
+namespace CodeChallenges;
+
+public class NumberSorter
+{
+    static int[] BubbleSort(int[] input) // Worst-case complexity: n^2
+    {
+        if (input == null) {return new int[] {};} // Needed to handle the null scenario.
+        var output = input;
+        bool loopAgain = true;
+
+        while (loopAgain == true)
+        {
+            loopAgain = false; // Priming.
+
+            for (int i = 0; i < input.Length - 1; i++) // Since we are looking ahead in the array, "- 1" is needed to avoid an out of bounds error.
+            {
+                if (output[i] > output[i+1])
+                {
+                    var temp = output[i];    //
+                    output[i] = output[i+1]; // Swap
+                    output[i+1] = temp;      //
+                    loopAgain = true;
+                }
+                //Console.WriteLine("[{0}]", string.Join(", ", output)); // Hack to print array to console.
+            }
+            // If we reach this point and loopAgain is false, then the numbers are in order.
+        }
+        return output;
+    }
+
+    static int[] MergeSort(int[] input) // Todo. Worst-case complexity: nlogn
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] InsertionSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] HeapSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] QuickSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] CountingSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] RadixSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] BucketSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] CombSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+    static int[] ShellSort(int[] input) // Todo.
+    {
+        var output = input;
+        return output;
+    }
+
+
+    static int[] Solve(int[] input)
+    {
+        if(input == null)
+        input = new int[0];
+
+        Array.Sort(input);
+        return input;
+    }
+
+    [TestCase(new int[] { 1, 2, 3, 5, 10 }, new int[] { 1, 2, 3, 10, 5 })]
+    [TestCase(new int[] { 2, 10, 20 }     , new int[] { 20, 2, 10 })]
+    [TestCase(new int[] { 2, 10, 20 }     , new int[] { 2, 20, 10 })]
+    [TestCase(new int[] { 2, 10, 20 }     , new int[] { 2, 10, 20 })]
+    [TestCase(new int[] { }               , new int[] { })]
+    [TestCase(new int[] { }               , null)]
+    public static void Test(int[] expectedOutput, int[] input)
+    {
+        Assert.AreEqual(expectedOutput, BubbleSort(input));
+    }
+
+}
