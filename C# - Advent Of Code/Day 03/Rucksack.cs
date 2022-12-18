1b93827da908e6fcc254c23f9296cc472c307c52
matt-cassinelli <67283034+matt-cassinelli@users.noreply.github.com>
2022-12-18 00:22:52 +0000

    Solve day 3

---
+++ C# - Advent Of Code/Day 03/Rucksack.cs
@@ -0,0 +1,18 @@
+﻿namespace AoC.Y2022.D03;
+
+class Rucksack
+{
+    public Rucksack(string items)
+    {
+        Items = new List<Item>();
+        foreach (char id in items)
+        {
+            Items.Add(new Item(id));
+        }
+    }
+
+    public List<Item> Items { get; }
+    private List<Item> compartment1 { get => Items.GetRange(0, Items.Count / 2); }
+    private List<Item> compartment2 { get => Items.GetRange(Items.Count / 2, Items.Count / 2); }
+    public Item CommonItem { get => compartment1.IntersectBy(compartment2.Select(i => i.Id), i => i.Id).First(); }
+}
